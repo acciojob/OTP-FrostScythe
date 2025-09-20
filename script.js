242f27b806dd3a8912aeefdf98a110bdc840de91
Ayush Maheshwari <111050424+FrostScythe@users.noreply.github.com>
2025-09-20 22:43:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 //your JS code here. If required.
 const codes = document.querySelectorAll(".code");
 
-    codes[0].focus(); // Focus first field on load
+codes[0].focus(); // Focus first field on load
 
-    codes.forEach((input, index) => {
-      input.addEventListener("input", (e) => {
-        const value = e.target.value;
-        if (value.match(/[0-9]/)) {
-          if (index < codes.length - 1) {
-            codes[index + 1].focus();
-          }
-        } else {
-          e.target.value = ""; // prevent non-numeric
-        }
-      });
+codes.forEach((input, index) => {
+  input.addEventListener("input", (e) => {
+    const value = e.target.value;
+    if (value.match(/[0-9]/)) {
+      if (index < codes.length - 1) {
+        codes[index + 1].focus();
+      }
+    } else {
+      e.target.value = ""; // prevent non-numeric
+    }
+  });
 
-      input.addEventListener("keydown", (e) => {
-        if (e.key === "Backspace") {
-          if (input.value === "") {
-            if (index > 0) {
-              codes[index - 1].focus();
-              codes[index - 1].value = "";
-            }
-          } else {
-            input.value = "";
-          }
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      if (input.value === "") {
+        if (index > 0) {
+          codes[index - 1].focus();
+          codes[index - 1].value = "";
         }
-      });
-    });
+      } else {
+        input.value = "";
+      }
+    }
+  });
+});
